b2336152e8ed958f510aa4d6101bb95b4dbeed3f
Tom <tgadacy@gmail.com>
2023-02-11 23:24:16 -0500

    Add conditional formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,17 +13,22 @@ export class AppComponent {
     poo: number = 0;
     hasPeed: boolean = false;
     hasPooped: boolean = false;
+    peeTime: Date;
+    pooTime: Date;
 
     constructor(private http: ConfigService) {}
 
     ngOnInit() {
         this.http.getLastPee().subscribe((data: Date) => {
-            this.pee = this.differenceInHours(new Date(data));
+            const dateTime = new Date(data);
+            this.pee = this.differenceInHours(dateTime);
+            this.peeTime = dateTime;
         })
 
-
         this.http.getLastPoo().subscribe((data: Date) => {
-            this.poo = this.differenceInHours(new Date(data));
+            const dateTime = new Date(data);
+            this.poo = this.differenceInHours(dateTime);
+            this.pooTime = dateTime;
         })
     }
 
@@ -33,16 +38,16 @@ export class AppComponent {
     }
 
     submitEntry() {
-        console.log(`Pee: ${this.hasPeed}`);
-        console.log(`Poo: ${this.hasPooped}`);
-        
         this.http.addEntry(this.hasPeed, this.hasPooped).subscribe((data: LastEntry) => {
+            const dateTime = new Date(data.dateTime);
             if (data.hasPeed) {
                 this.pee = 0;
+                this.peeTime = dateTime;
             }
             
             if (data.hasPooped) {
                 this.poo = 0;
+                this.pooTime = dateTime;
             }
             // start countdown to update pee and poo
         });
